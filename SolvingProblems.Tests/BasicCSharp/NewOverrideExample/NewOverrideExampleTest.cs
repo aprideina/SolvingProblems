55c29e6161fd900d37ffd984fae15213da21168b
PrideinaAnastasia <nastya-999-91@mail.ru>
2021-05-22 12:41:14 +0300

    New, Override keywords examples and tests

---
+++ SolvingProblems.Tests/BasicCSharp/NewOverrideExample/NewOverrideExampleTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using SolvingProblems.BasicCSharp.NewOverrideExample;
+
+namespace SolvingProblems.Tests.BasicCSharp.NewOverrideExample
+{
+    public class NewOverrideExampleTest
+    {
+        [Test]
+        public void Test()
+        {
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+            BaseClass bcdc = new DerivedClass();
+
+            Assert.That(bc.BaseMethod(), Is.EqualTo(MethodType.Base));
+            Assert.That(dc.BaseMethod(), Is.EqualTo(MethodType.Base));
+            Assert.That(dc.DerivedMethod(), Is.EqualTo(MethodType.Derived));
+            Assert.That(bcdc.BaseMethod(), Is.EqualTo(MethodType.Base));
+        }
+
+        [Test]
+        public void BothMethodsTest()
+        {
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+            BaseClass bcdc = new DerivedClass();
+
+            Assert.That(bc.BothMethod(), Is.EqualTo(MethodType.Base));
+            Assert.That(dc.BothMethod(), Is.EqualTo(MethodType.Derived));
+            Assert.That(bcdc.BothMethod(), Is.EqualTo(MethodType.Base));
+        }
+
+
+        [Test]
+        public void OverridedMethodsTest()
+        {
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+            BaseClass bcdc = new DerivedClass();
+
+            Assert.That(bc.OverridedMethod(), Is.EqualTo(MethodType.Base));
+            Assert.That(dc.OverridedMethod(), Is.EqualTo(MethodType.Overrided));
+
+            Assert.That(bcdc.OverridedMethod(), Is.EqualTo(MethodType.Overrided));
+        }
+    }
+}
---
+++ SolvingProblems/BasicCSharp/NewOverrideExample/NewOverrideExample.cs
@@ -0,0 +1,49 @@
+﻿namespace SolvingProblems.BasicCSharp.NewOverrideExample
+{
+    public enum MethodType
+    {
+        Base,
+        Derived,
+        Overrided
+    }
+
+    /// <summary>
+    /// Examples for better understanding new/override keywords
+    /// https://docs.microsoft.com/ru-ru/dotnet/csharp/programming-guide/classes-and-structs/knowing-when-to-use-override-and-new-keywords
+    /// </summary>
+    public class BaseClass
+    {
+        public MethodType BaseMethod()
+        {
+            return MethodType.Base;
+        }
+
+        public MethodType BothMethod()
+        {
+            return MethodType.Base;
+        }
+
+        public virtual MethodType OverridedMethod()
+        {
+            return MethodType.Base;
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public MethodType DerivedMethod()
+        {
+            return MethodType.Derived;
+        }
+
+        public new MethodType BothMethod()
+        {
+            return MethodType.Derived;
+        }
+
+        public override MethodType OverridedMethod()
+        {
+            return MethodType.Overrided;
+        }
+    }
+}
